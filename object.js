523281a77fe3a5c4884feec36cbb07b7b362fe46
arifwdn <muhammadarifwardana01@gmail.com>
2022-04-20 18:08:30 +0800

    commit "OK"

--- object.js
+++ object.js
@@ -8,6 +8,7 @@
 //         console.log("Hallo " + this.nama + ", Selamat Makan!");
 //     }
 // };
+// Tidak efektif untuk object yang berulang/banyak
 
 // 2. Object Function Declaration
 // function Mahasiswa(nama, energi) {
@@ -24,6 +25,32 @@
 //     }
 //     return Mhs;
 // }
+// Duplikasi fungsi yang tidak terpakai tetap terulang
+// Object.create()
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log("Hallo " + this.nama + ", Selamat Makan!");
+//     },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log("Hallo " + this.nama + ", Selamat Bermain!");
+//     },
+//     tidur: function (jam) {
+//         this.energi += jam * 2;
+//         console.log("Hallo " + this.nama + ", Selamat Tidur!");
+//     }
+// }
+
+// function Mahasiswa(nama, energi) {
+//     //menghubungkan object parent untuk mengelola memori lebih efektif
+//     let Mhs = Object.create(methodMahasiswa);
+//     Mhs.nama = nama;
+//     Mhs.energi = energi;
+
+//     return Mhs;
+// }
+// let arif = Mahasiswa("Arif", 10);
 
 // 3. Constructor Function =>new
 // function Mahasiswa(nama, energi) {
@@ -41,4 +68,42 @@
 
 // let arif = new Mahasiswa("Arif", 10);
 
-// 4. Object.create
+// Prototype
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log("Hallo " + this.nama + ", Selamat Makan!");
+// }
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     console.log("Hallo " + this.nama + ", Selamat Bermain!");
+// }
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energi += jam * 2;
+//     console.log("Hallo " + this.nama + ", Selamat Tidur!");;
+// }
+
+// let arif = new Mahasiswa("Arif", 10);
+// Versi OOP atau Class Object
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+    makan(porsi) {
+        this.energi += porsi;
+        return "Hallo " + this.nama + ", Selamat Makan!";
+    }
+    main(jam) {
+        this.energi -= jam;
+        return "Hallo " + this.nama + ", Selamat Bermain!";
+    }
+    tidur(jam) {
+        this.energi += jam * 2;
+        return "Hallo " + this.nama + ", Selamat Tidur!";
+    }
+}
+let arif = new Mahasiswa("arif", 10);
